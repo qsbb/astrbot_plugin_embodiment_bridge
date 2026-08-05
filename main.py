44d5751b4d2269e850754d51e7d88cdba8e42149
qsbb <951352430@qq.com>
2026-08-05 23:32:24 +0800

    fix: release persona integration as v0.4.2

--- main.py
+++ main.py
@@ -41,7 +41,7 @@ from .transport.http_sse import HttpSseTransport, PLUGIN_NAME, TransportConfig
 from .transport.pairing import PairingHttpApi
 
 
-__version__ = "0.5.0"
+__version__ = "0.4.2"
 
 
 class QuestAvatarBridgePlugin(Star):
